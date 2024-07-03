130fb67d052352712f707634ff52cd2e952dad61
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-07-02 22:34:52 -0300

    Bootstrap

---
+++ src/main/kotlin/Aula01.kt
@@ -0,0 +1,41 @@
+package org.example
+
+import java.math.BigDecimal
+
+fun main() {
+    println("Hello World!")
+
+    // val cria constantes
+    // var cria variaveis comuns
+    val number = 1
+    //number = 12
+    var number2 = 1
+    number2 = 2
+    println(number)
+    println(number2)
+
+    // tipos primitivos do kotlin
+
+
+    // Byte short int e long são todos inteiros
+    // mas o numero de bytes que eles usam é diferente
+    var inteiro: Int
+    var byte: Byte
+    var short: Short
+    var long: Long
+    // Float e Doubles representam numeros com pontos flutuantes mas com difrença de precisão
+    var float: Float
+    var double: Double
+    // O tipo big decimal é usado parar tratar com dinheiro
+    var bigDecimal: BigDecimal
+    //Char, String
+    var char: Char
+    var string: String
+    //Booleano
+    var booleano: Boolean
+    //Arrays
+    var array: Array<Int>
+
+
+
+}
---
+++ src/main/kotlin/Aula02.kt
@@ -0,0 +1,29 @@
+package org.example
+
+fun main(){
+    //Operadores
+
+    // Atribuição =
+
+    var numero = 1
+
+    println(numero)
+
+    // Adição e concatenação
+
+    var numero2 = numero + 3
+
+    println(numero2)
+    println("String 1 " + "String 2")
+
+    //Subtração - multiplicação *
+
+    println("1 - 2 = ${1-2}") // da para interpolar valores assim
+    println("2 * 3 ${2*3}")
+
+    //operadores de comparação comuns == <= >= < >
+
+    println("${"string1".equals("String 2")}")
+
+
+}
